28a2772eea6b54c8d18d9540577aa87dd0659c55
THMC04 <127440499+THMC04@users.noreply.github.com>
2023-07-28 20:58:57 +0100

    Added the User class

---
+++ main.py
@@ -0,0 +1,75 @@
+import sqlite3
+import os
+from colorama import Fore, Style
+
+
+class User():
+    def __init__(self, name, con):
+        self.name = name
+        self.con = con
+    
+
+    def create_table(self):
+
+        cur = self.con.cursor()
+        
+        sql_c = f"""
+        CREATE TABLE IF NOT EXISTS {self.name} (
+        Number VARCHAR(12),
+        Name TEXT,
+        CountryCode VARCHAR(4),
+        PRIMARY KEY (Number)
+        );
+        """ 
+        cur.execute(sql_c)
+        self.con.commit()
+
+    
+    def add_entry(self, number, name, code):
+        
+        cur = self.con.cursor()
+
+        sql_c = f"""
+        INSERT INTO {self.name} VALUES (?, ?, ?);
+        """
+        try:
+            cur.execute(sql_c,(number,name,code))
+        except sqlite3.IntegrityError:
+            print(f"{Fore.RED}Failed to add {number} to list.\nCAUSE: Already added{Style.RESET_ALL}")
+            return False
+        
+        self.con.commit()
+    
+
+    def remove_entry(self, number):
+        
+        cur = self.con.cursor()
+
+        sql_c = f"""
+        DELETE FROM {self.name} WHERE Number = ?;
+        """
+
+        cur.execute(sql_c, (number,))
+        self.con.commit()
+    
+    def replace_entry(self, number, code, new_name):
+
+        self.remove_entry(number)
+        self.add_entry(number, new_name, code)
+    
+
+    def import_data(self, file):
+       
+        with open(file, "r", encoding="utf-8") as f:
+            
+            # Amount of entries successfully added
+            total = 0
+            
+            for line in f.readlines():
+                check = True
+                code, number, name = line.split(" ").strip()
+                check = self.add_entry(number, name, code)
+                if check:
+                    total += 1
+        
+        return total
