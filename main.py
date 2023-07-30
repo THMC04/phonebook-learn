282a31a519d0bf18db7952bc5b161873cb1244e2
THMC04 <127440499+THMC04@users.noreply.github.com>
2023-07-30 17:06:49 +0100

    Add search operation, user deletion and bug fixes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sqlite3
 import os
 import sys
 from colorama import Fore, Style
-
+import tabulate
 
 class User():
     def __init__(self, name, con):
@@ -25,6 +25,14 @@ class User():
         cur.execute(sql_c)
         self.con.commit()
 
+    def delete_table(self):
+
+        cur = self.con.cursor()
+        
+        sql_c = f"DROP TABLE {self.name};"
+
+        cur.execute(sql_c)
+        self.con.commit()
     
     def add_entry(self, number, name, code):
         
@@ -84,10 +92,10 @@ class User():
         cur = self.con.cursor()       
         
         if number != None:
-            sql_c = f"""SELECT Name FROM {self.name} Where Number ==  ?;"""
+            sql_c = f"""SELECT Name, CountryCode, Number FROM {self.name} Where Number ==  ?;"""
             res = cur.execute(sql_c, (number,))
         elif name != None:
-            sql_c = f"""SELECT Number FROM {self.name} Where Name ==  ?;"""
+            sql_c = f"""SELECT Name, CountryCode, Number FROM {self.name} Where Name ==  ?;"""
             res = cur.execute(sql_c, (name,))
 
         values = res.fetchall()
@@ -103,12 +111,7 @@ class User():
         res = cur.execute(sql_c)
         values = res.fetchall()
         
-        final_values = []
-        for elem in values:
-            inter_values = []
-            for value in elem:
-                inter_values.append(value)
-            final_values.append(inter_values)
+        final_values = tpl_lst_to_lst(values)
         
         return final_values
         
@@ -128,19 +131,44 @@ def return_users(con):
     
     res = cur.execute(sql_c)
     values = res.fetchall()
-    final_values = tpl_lst_to_lst(values)
+    
+    final_values = []
+    
+    for elem in values:
+        final_values.append(elem[0])
     
     return final_values
-
+    
 
 def tpl_lst_to_lst(tuple_list):
     
     final_values = []
-    
+
     for elem in tuple_list:
-        final_values.append(elem[0])
-    
+        inter_values = []
+        for value in elem:
+            inter_values.append(value)
+        final_values.append(inter_values)
+        
     return final_values
+
+
+def input_check(choice, final_inp):
+    
+    try:
+                
+        choice = int(choice)
+            
+    except ValueError:
+
+        print(f"\n{Fore.RED}{choice} is not a valid option - (1 - {final_inp}){Style.RESET_ALL}\n")
+        return False                               
+            
+    if choice not in range(1, final_inp + 1):
+        print(f"\n{Fore.RED}{choice} is not a valid option - (1 - {final_inp}){Style.RESET_ALL}\n")
+        return False
+
+    return True, choice
 
 
 def new_user(con):
@@ -165,7 +193,7 @@ def boot_up(con):
 
     if len(users) == 0:
         
-        print(f"{Fore.BLUE}Welcome new user!\n{Style.RESET_ALL}\nPlease create a new user")
+        print(f"\n{Fore.BLUE}Welcome!\n{Style.RESET_ALL}\n{Fore.YELLOW}No Users found. Please create a new user{Style.RESET_ALL}\n")       
         current_user = new_user(con)
         return current_user
     
@@ -174,54 +202,138 @@ def boot_up(con):
         while True:
             
             true_size = len(users)
-            total_size = true_size + 2
+            total_size = true_size + 3
             
             print(total_size)
 
             print(f"\n{Fore.BLUE}Welcome back!{Style.RESET_ALL}\n")
-            print(f"{Fore.CYAN}Please choose an user (1 - {total_size}):{Style.RESET_ALL}\n")
+            print(f"{Fore.CYAN}Please choose a user (1 - {total_size}):{Style.RESET_ALL}\n")
             
             for i in range(true_size):
                 print(str(i + 1) + f" - {users[i]}\n")
-            print(str(total_size - 1) + " - Create new user\n")
+            print(str(total_size - 2) + " - Create new user\n")
+            print(str(total_size - 1) + " - Delete a user\n")
             print(str(total_size) + " - Exit\n")
 
             choice = input()
             
-            try:
-                
-                choice = int(choice)
-            
-            except ValueError:
+            flag, choice = input_check(choice, total_size)
 
-                print(f"{Fore.RED}{choice} is not a valid option - (1 - {total_size}){Style.RESET_ALL}\n")
-                continue                               
+            if flag == False:
+                continue
             
-            if choice not in range(1, total_size + 1):
-
-                print(f"{Fore.RED}{choice} is not a valid option - (1 - {total_size}){Style.RESET_ALL}\n")
-                continue 
+            if choice == total_size:
+                stop(con)    
+            elif choice == total_size - 1:
+                delete_user(users, con)
+            elif choice == total_size - 2:
+                current_user = new_user(con)
+                return current_user 
             else:
+                current_user = User(users[choice - 1], con)
+                return current_user
+
+
+def delete_user(users, con):
+    
+    true_size = len(users)
+    total_size = true_size + 1
+    
+    while True:
+        print(f"{Fore.CYAN}Choose a user to delete (1 - {total_size}):{Style.RESET_ALL}\n")     
                 
-                if choice == total_size:
-                    stop(con)
-                
-                elif choice == total_size - 1:
-                    current_user = new_user(con)
-                    return current_user
-                else:
-                    current_user = User(users[choice - 1], con)
-                    return current_user
+        for i in range(true_size):
+            print(str(i + 1) + f" - {users[i]}\n")
+        print(f"{total_size} - Return")
+    
+        del_user = input("\n")
+
+        flag, del_user = input_check(del_user, total_size)
+
+        if flag == False:
+            continue
+
+        if del_user == true_size + 1:
+            main()
+        else:
+            sel_user = User(users[del_user - 1], con)
+            sel_user.delete_table()
+            main()
+
+
+def operations(user):
+
+    while True:
+        
+        print(f"\n{Fore.CYAN}Please choose an operation (1 - 7):{Style.RESET_ALL}\n")
+        print("1 - Show full phonebook")
+        print("2 - Look up a number or name")
+        print("3 - Add a new number")
+        print("4 - Remove a number")
+        print("5 - Change a number's associated name")
+        print("6 - Insert large amount of numbers")
+        print("7 - Return\n")
+
+        choice = input()
+        
+        flag, choice = input_check(choice, 7)
+        
+        if flag == False:
+            continue
+        
+        match choice:
+            case 1:
+                pass
+            case 2:
+                search_phonebook(user)
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                pass
+            case 6:
+                pass
+            case 7:
+                return
+
+
+def search_phonebook(user):
+
+    number = True
+    print(f"\n{Fore.CYAN}Please input a name or a number:{Style.RESET_ALL}\n")
+
+    choice = input()
+    
+    try:
+        int(choice)
+    except ValueError:
+        number = False
+    
+    if number:
+        res = user.look_up(number = choice)
+    else:
+        res = user.look_up(name = choice)
+
+    headers = ["Name", "Country Code", "Number"]
+    print(res)
+    print(tabulate.tabulate(res, headers, tablefmt="pretty"))
+
+    input("\nPress ENTER when done\n")
+
+
 
 
 def main():
     con = sqlite3.connect("test.db")
     
-    current_user = boot_up(con)
-
-    con.close()
+    while True:
+    
+        current_user = boot_up(con)
+        operations(current_user)
+    1
 
 if __name__ == "__main__":
 
     main()
-    
+
